41f01af997a6d32eb6215e97e37e2151f1196732
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2023-04-12 20:28:08 -0400

    update home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -109,6 +109,81 @@ function Learners(){
   )
 }
 
+function Mentors(){
+  return (
+    <div className="bg-sky-900 py-16 sm:pt-24 sm:pb-20">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-3xl lg:mx-0">
+          <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">A Home Base for Mentors</h2>
+          <p className="mt-6 text-lg leading-8 text-gray-300">
+            Connect with mentees and learners through our community, and get support from your fellow mentors!
+            All of us at Clojure Camp love teaching, and we also know that it can be a solitary and taxing endeavor.
+            We're creating a sustainable mentor community where we can learn from each other and offer each other
+            camaraderie.
+          </p>
+        </div>
+     </div>
+    </div>
+  )
+}
+
+function OurVision() {
+return (
+    <div className="bg-slate-800 py-16 sm:pt-24 sm:pb-20">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-3xl lg:mx-0 text-gray-300">
+          <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">Our Vision</h2>
+          <p className="mt-6 text-lg leading-8">
+            What if Clojure became the most beginner-friendly language on the planet?
+          </p>
+
+          <p className="mt-6 text-lg leading-8">
+Clojure Camp aims to be the “Clojure Welcoming Committee,” the definitive
+starting point for junior developers who want to learn Clojure. We will help
+these learners by guiding them to resources that can help them become proficient
+at Clojure and build relationships in the community, from friendships to
+collaborations to even employment.
+          </p>
+
+          <p className="mt-6 text-lg leading-8">
+Camps can’t function without organizers and teachers. Clojure Camp aims to be a
+resource for mentors and teachers in the community by:
+          </p>
+
+          <ul>
+            <li>Providing a distribution channel for the learning resources they create</li>
+            <li>Providing tools for pairing learners with mentors</li>
+            <li>Hosting events to socialize, give each other moral support, and help each other get unstuck</li>
+            <li>Developing and sharing resources for effective execution of learning projects</li>
+          </ul>
+
+          <p className="mt-6 text-lg leading-8">
+            We believe that learning Clojure is a great long-term investment
+            because it helps people learn a better way of approaching
+            programming, and because it brings creative satisfaction. We want to
+            make it accessible to more people.
+          </p>
+          <p className="mt-6 text-lg leading-8">
+            We believe that <em>how</em> we operate is just as important as what
+            we do. We aim to create a positive, beginner-friendly, inclusive
+            environment for learners and leaders alike. We want all who get
+            involved with Clojure Camp to leave with a sense of having been
+            appreciated, respected, and supported.
+          </p>
+          <p className="mt-6 text-lg leading-8">
+            In particular, we want to unambiguously communicate that people from
+            marginalized groups can participate in a safe environment, where
+            they'll be welcomed and won't face harassment, discrimination, or
+            other forms of exclusion. We want to be diligent about creating an
+            environment that delivers on that promise.
+          </p>
+
+
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default function Home() {
   return (
@@ -124,16 +199,22 @@ export default function Home() {
               <p className="mt-2 text-3xl font-bold tracking-tight text-orange-300 sm:text-4xl">
                 The community you need for your programming journey
               </p>
-              <p className="mt-6 text-lg leading-8 text-white">
 
+              <p className="mt-6 text-lg leading-8 text-white">
                 Clojure Camp helps new developers build a secure foundation for
                 professional programming success &mdash; with Clojure! We
                 connect you with an inclusive, kind, supportive, and fun
                 community of mentors and fellow learners, and with the best
                 resources for learning the best programming language.
-
-
               </p>
+
+              <p className="mt-6 mb-6 text-lg leading-8 text-white">
+                Clojure Camp is rooted in an equity ethos: it's open to all
+                while prioritizing the experience of those who face the most
+                hurdles in tech. Our Community is a place where Women, BIPOC
+                folks, and LGBTQ+ folks belong.
+              </p>
+
 
               {GetNotified()}
             </div>
@@ -143,7 +224,7 @@ export default function Home() {
           <div class="justify-center text-center">
                              <img
                      class="max-w-xs md:max-w-xs sm:max-w-xs lg:max-w-xs xl:max-w-sm mx-auto"
-                     src="/img/ClojureCamp-MasterLogo-RGB-FullColor.png"
+                     src="/img/ClojureCamp-SecondaryLogo-RGB-FullColor.png"
                      alt=""
           />
           </div>
@@ -156,6 +237,8 @@ export default function Home() {
      {/* break */}
 
         { Learners() }
+        { Mentors() }
+        { OurVision() }
 
     { /* <Features/> */ }
     </div>

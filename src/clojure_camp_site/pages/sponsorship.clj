ae43b438607366028ab1e571ec9dfaf47c5762b9
Austin M <austin.meier@hotmail.com>
2025-06-12 09:56:43 -0600

    styling less ugly

--- src/clojure_camp_site/pages/sponsorship.clj
+++ src/clojure_camp_site/pages/sponsorship.clj
@@ -6,20 +6,22 @@
 
 (defn body []
   [:div
-   {:class "overflow-hidden bg-gray-900 text-white py-4 px-4 bg-gradient-to-t from-slate-800 via-slate-800 to-slate-700"}
+   {:class "overflow-hidden bg-gray-900 text-white bg-gradient-to-t from-slate-800 via-slate-800 to-slate-700"}
 
    [:div
-    {:class "justify-center text-center"}
+    {:class "justify-center text-center py-4 px-4"}
     [:h1
-     {:class "py-4 text-3xl font-bold tracking-tight sm:text-4xl"}
+     {:class "py-6 text-orange-300 text-3xl font-bold tracking-tight sm:text-4xl"}
      "Sponsorship"]
     [:img
      {:class
       "max-w-xs md:max-w-xs sm:max-w-xs lg:max-w-xs xl:max-w-sm mx-auto",
       :src "/assets/ClojureCamp-SecondaryLogo-RGB-FullColor.png",
       :alt ""}]]
-   [:div.prose
-    (-> (io/resource "clojure_camp_site/sponsorship.md")
-        slurp
-        md/md-to-html-string
-        h/raw)]])
+   [:div
+    {:class "bg-gray-900 py-2 px-24"}
+    [:div.prose
+     (-> (io/resource "clojure_camp_site/sponsorship.md")
+         slurp
+         md/md-to-html-string
+         h/raw)]]])
